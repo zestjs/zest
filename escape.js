731ffec592a72f660b23107b43f4b021e1d42e23
guybedford <guybedford@Guys-MacBook-Air-2.local>
2012-10-31 13:05:41 +0200

    added html escaping

--- escape.js
+++ escape.js
@@ -1,13 +1,21 @@
  //routes resolve to pages
 //$z.App should me in Main
 define(function() {
+  //arguments after mode are passed as successive arguments to the escape function
   var esc = function(text, mode) {
-    return esc[mode](text);
+    var args = Array.prototype.splice.call(arguments, 2, arguments.length - 2);
+    return esc[mode](args.unshift(text));
   }
-  esc.px = function(text) {
-    var num = parseInt(text);
-    return num + 'px';
+  //useful for css sizes etc. Suffix allows for dimensions eg px
+  esc.num = function(text, suffix, nanValue) {
+    var num = parseFloat(text);
+    if (isNaN(num))
+      return nanValue || '';
+    else
+      return num + '' + suffix;
   }
+  
+  //html attributes
   esc.attr = function(attr) {
     return attr
       .replace(/</g, '&lt;')
@@ -15,14 +23,32 @@ define(function() {
       .replace(/"/g, '&quot;')
       .replace(/'/g, '&#39;');
   }
-  esc.int = function(num) {
-    return parseInt(num);
+  
+  //text only
+  esc.text = function(test) {
+    return text
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
   }
-  esc.html = function(html) {
+  
+  //filtered html
+  esc.html = function(html, allowedTags) {
+    allowedTags = allowedTags || ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span', 'div', 'a', 'table', 'tr', 'td', 'tbody', 'thead', 'section', 'strong', 'em', 'blockquote', 'img', 'hr', 'br'];
     
+    //loop through all html tags, and escape non-allowed
+    var index;
+    var openingTag = /<[^\s><\/]+/g;
+    var closingTag = /<\/[^\s><\/]+/g;
+    while ((match = openingTag.exec(html))) {
+      //if not an allowed tag, escape
+      if (allowedTags.indexOf(match[0].substr(1)) == -1)
+        html = html.substr(0, match.index) + '&lt;' + html.substr(match.index + 1);
+    }
+    while ((match = closingTag.exec(html)))
+      if (allowedTags.indexOf(match[0].substr(2)) == -1)
+        html = html.substr(0, match.index) + '&lt;' + html.substr(match.index + 1);
+    return html;
   }
-  esc.filter_html = function(html, tagFilter) {
-    
-  }
+  
   return esc;
 });
