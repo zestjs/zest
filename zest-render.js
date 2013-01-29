d23f841020c30cc0515e538110a11db1456d80e2
guybedford <guybedford@gmail.com>
2013-01-29 13:09:29 +0200

    class name adjustments

--- zest-render.js
+++ zest-render.js
@@ -478,7 +478,7 @@
       
       var _id = options.id;
       var _type = component.type;
-      var _class = component.className + (options.className ? ' ' + options.className);
+      var _class = component.className + (options.className && component.className ? ' ' + options.className : options.className || '');
 
       if (_type && _type.substr(0, 1).toUpperCase() != _type.substr(0, 1))
         throw 'Type names must always start with an uppercase letter.';
